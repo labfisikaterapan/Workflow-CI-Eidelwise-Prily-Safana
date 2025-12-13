29e0cd10c594afd1a51604768b28be0b1398d363
Eidelwise Prily Safana <eidelwise@example.com>
2025-12-13 14:31:39 +0700

    Fix conda: use pip env

---
+++ PUSH.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\mriva\OneDrive\Dokumen\New folder\SMSL_Eidelwise Prily Safana\GitHub_Repos\Workflow-CI-Eidelwise-Prily-Safana"
+git add .
+git commit -m "Fix CONDA ERROR: Use python_env.yaml for pip-based MLflow"
+git push origin main
+pause
